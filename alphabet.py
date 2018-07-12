d9edb63125edfa7d9d12d9e267ca8928c62cbe89
nharini27 <41140729+nharini27@users.noreply.github.com>
2018-07-12 14:36:09 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -2,6 +2,6 @@
 list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 alp=raw_input()
 if alp in list:
-	print("Yes")
+	print("Alphabet")
 else:
 	print("No")

1f95ef35b1b0469083c3b28c2ecf1939d7016e17
nharini27 <41140729+nharini27@users.noreply.github.com>
2018-07-12 14:13:46 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
 N=input()
 if(N=='a' or N=='e' or N=='i' or N=='o' or N=='u'):
-	print("Vowels")
+	print("Vowel")
 elif(N=='b' or N=='c'or N=='d' or N=='h' or N=='f'or N=='g' or  N=='j' or  N=='k' or  N=='l' or  N=='m' or  N=='n' or  N=='p' or  N=='q' or  N=='r' or  N=='s' or  N=='t' or  N=='v' or  N=='w' or  N=='x' or  N=='y' or  N=='z' ):
-	print("Consonants")
+	print("Consonant")
 else:
-	print("invalid")
+	print("Invalid")
